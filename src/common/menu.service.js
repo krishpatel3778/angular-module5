4a00bfcd13c1cff40903e162ae2df8a0efc4b8f2
krishpatel3778 <patelrkrish@icloud.com>
2022-12-03 14:29:26 -0500

    done

--- src/common/menu.service.js
+++ src/common/menu.service.js
@@ -12,6 +12,7 @@ function MenuService($http, ApiPath) {
   service.getCategories = function () {
     //ApiPath + '/categories.json'
     return $http.get('https://www.freepatentsonline.com/result.html?p=1&edit_alert=&srch=xprtsrch&query_txt=PEX%2Fsmith+AND+ABST%2FGlass%0D%0A&uspat=on&date_range=all&stemming=on&sort=relevance&search=Search').then(function (response) {
+      console.log("here")
       console.log(response.data)
       return response.data;
     });
