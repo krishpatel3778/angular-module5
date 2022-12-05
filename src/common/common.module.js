dd97070de99befdf0c4222064ad62575bac38aff
krishpatel3778 <101026727+krishpatel3778@users.noreply.github.com>
2022-12-05 15:53:16 -0500

    Update common.module.js

--- src/common/common.module.js
+++ src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://patel3778-data.herokuapp.com')
+.constant('ApiPath', 'https://patel3778-data.herokuapp.com/')
 .config(config);
 
 config.$inject = ['$httpProvider'];
